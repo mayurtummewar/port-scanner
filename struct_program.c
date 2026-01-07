2429b22e5fbe3413c9e7d9f30885f23b66b20802
mayurtummewar <mayurtummewar@gmail.com>
2026-01-07 11:24:26 +0530

    scanning one ip

--- struct_program.c
+++ struct_program.c
@@ -1,19 +1,35 @@
-#include<stdio.h>
-
-struct Server {
-	char ip[16];
-	int port;
-};
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
 
 int main() {
-	struct Server s;
-	struct Server *ptr = &s;
 
-	snprintf(ptr->ip, sizeof(ptr->ip), "127.0.0.1");
-	ptr->port = 80;
+	int sock = socket(AF_INET, SOCK_STREAM, 0);
+	
+	if (sock < 0) {
+		perror("Socket creation failed");
+		return 1;
+	}
 
-	printf("Server IP    : %s\n", ptr->ip);
-	printf("Server port  : %d\n", ptr->port);
+	struct sockaddr_in server;
+	
+	server.sin_family = AF_INET;
+	server.sin_port = htons(80);
 
+	if (inet_pton(AF_INET, "142.250.195.14", &server.sin_addr) <= 0) {
+		perror("Invalid Address");
+		close(sock);
+		return 1;
+	}
+
+	if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0) {
+		perror("Connection Unsuccessful\n");
+	} else {
+		printf("Connection Successful!\n");
+	}
+
+	close(sock);
 	return 0;
 }
