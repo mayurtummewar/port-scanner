41d7cf712156455b256d7dcd118b1518b4ce2e14
mayurtummewar <mayurtummewar@gmail.com>
2026-01-08 11:27:34 +0530

    takes user inputs

--- program.c
+++ program.c
@@ -1,34 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 
-int main() {
+int main(int argc, char *argv[]) {
 
 	struct sockaddr_in target;
+	memset(&target, 0, sizeof(target));
 	int sock;
-	int port;
-	
+	int port, start_port, end_port;
+	if (argc != 4) {
+		printf("error : 3 arguments expected");
+		return 1;
+	}
+
+	start_port = strtol(argv[2], NULL, 10);
+	if (start_port < 1 || start_port > 65535) {
+		perror("invalid start_port");
+		return 1;
+	}
+	end_port = strtol(argv[3], NULL, 10);
+	if (end_port < 1 || end_port > 65535) {
+		perror("invalid end_port");
+		return 1;
+	}
+
 	target.sin_family = AF_INET;
 
-	inet_pton(AF_INET, "127.0.0.1", &target.sin_addr);
-	printf("scanning ports on 127.0.0.1...\n");
-
-	for (port = 1; port <= 1024; port++) {
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	
-	if (sock < 0) {
-		perror("Socket creation failed");
-		continue;
+	if (inet_pton(AF_INET, argv[1], &target.sin_addr) != 1) {
+		perror("invalid ip");
+		return 1;
 	}
+	printf("scanning from port %d to port %d on ip : %s\n", start_port, end_port, argv[1]);
 
-	target.sin_port = htons(port);
-	if (connect(sock, (struct sockaddr *)&target, sizeof(target)) == 0) {
-		printf("Port %d is open\n", port);
-	}
+	for (port = start_port; port <= end_port; port++) {
+		sock = socket(AF_INET, SOCK_STREAM, 0);
+		
+		if (sock < 0) {
+			perror("Socket creation failed");
+			continue;
+		}
 
-	close(sock);
+		struct timeval timeout;
+		timeout.tv_sec = 1;
+		timeout.tv_usec = 0;
+		
+		setsockopt(sock, SOL_SOCKET, SO_SNDTIMEO, &timeout, sizeof(timeout));
+
+		target.sin_port = htons(port);
+		if (connect(sock, (struct sockaddr *)&target, sizeof(target)) == 0) {
+			printf("Port %d is open\n", port);
+		}
+
+		close(sock);
 	}
 	return 0;
 }
